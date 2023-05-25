a46aeda5d460a4b8aa67792e8073dd5092aa5aab
irajmoradi <irajjmoradi@gmail.com>
2023-05-25 16:27:40 -0700

    new python just dropped, 10/10 would reccomend

--- jsonpython.py
+++ jsonpython.py
@@ -14,12 +14,28 @@ for x in range(len(questions)):
     print(questions[x], answer[x])
 
 promptss = ["answer this latin", "latin am i right?"]
+outerdict = {}
+outerdict["stuff"] = []
+
 
 for x in range(len(questions)):
+    innerdict = {}
+    innerdict["question"] = questions[x]
+    innerdict["answer"] = answer[x]
+    innerdict["prompt"] = []
+    innerdict["code"] = []
     for prompts in promptss:
+        stringg = (prompts + " " + questions[x]) 
         string = (prompts + " " + questions[x] + " " + "[ANSWER] ") 
         newstring = 'argmax"' + string + '" from "openai/text-ada-001"'
         #create json file for code
-        finalname = str(x) + prompts + ".lmql"
-        f = open(finalname, "w")
-        f.write(newstring)
+        innerdict["prompt"].append(stringg)
+        innerdict["question"] = questions[x]
+        innerdict["code"].append(newstring)
+        #finalname = str(x) + prompts + ".txt"
+        #f = open(finalname, "w")
+        #f.write(newstring)
+    outerdict["stuff"].append(innerdict)  
+with open("sample.json", "w") as outfile:
+    json.dump(outerdict, outfile)
+    
