736aa8a5739e8fc03100e5e75ee44470eb0ea247
irajmoradi <irajjmoradi@gmail.com>
2023-05-25 17:09:00 -0700

    new push

--- jsonpython.py
+++ jsonpython.py
@@ -1,41 +1,55 @@
 import json
-with open('jsonstuff.json', 'r') as f:
+
+#nested for loops
+
+with open('prompts.json', 'r') as f:
     data = json.load(f)
+promptss = data
 #print(data)
+allquestions = []
 questions = []
+allanswers = []
 answer  = []
-for x in range(len(data['questions'])):
-    
-    questions.append(data['questions'][x]["question"])
+for file_name in ["jsona.json", "jsonb.json", "jsonc.json"]
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    questions = []
+    answer = []
+    for x in range(len(data['questions'])):
+        questions.append(data['questions'][x]["question"])
+        answer.append(data['questions'][x]["answer"])
+    allanswers.append(answer)
+    allquestions.append(questions)
+print(allquestions)
 
-    answer.append(data['questions'][x]["answer"])
-
-for x in range(len(questions)):
-    print(questions[x], answer[x])
-
-promptss = ["answer this latin", "latin am i right?"]
+#promptss = ["answer this latin", "latin am i right?"]
 outerdict = {}
 outerdict["stuff"] = []
 
 
-for x in range(len(questions)):
-    innerdict = {}
-    innerdict["question"] = questions[x]
-    innerdict["answer"] = answer[x]
-    innerdict["prompt"] = []
-    innerdict["code"] = []
-    for prompts in promptss:
-        stringg = (prompts + " " + questions[x]) 
-        string = (prompts + " " + questions[x] + " " + "[ANSWER] ") 
-        newstring = 'argmax"' + string + '" from "openai/text-ada-001"'
+z = 0
+index = ["Quiz A", "Quiz B", "Quiz C"]
+for parts in len(range(3)):
+    letter = index[z]
+    z += 1
+    for x in range(len(allquestions[parts])):
+        innerdict = {}
+        innerdict["question"] = allquestions[parts][x]
+        innerdict["answer"] = allanswers[parts][x]
+        innerdict["prompt"] = []
+        innerdict["code"] = []
+        for prompts in promptss[letter]:
+            stringg = (prompts + " " + allquestions[parts][x]) 
+            string = (prompts + " " + allquestions[parts][x] + " " + "[ANSWER] ") 
+            newstring = 'argmax"' + string + '" from "openai/text-ada-001"'
         #create json file for code
-        innerdict["prompt"].append(stringg)
-        innerdict["question"] = questions[x]
-        innerdict["code"].append(newstring)
+            innerdict["prompt"].append(stringg)
+            innerdict["question"] = allquestions[parts][x]
+            innerdict["code"].append(newstring)
         #finalname = str(x) + prompts + ".txt"
         #f = open(finalname, "w")
         #f.write(newstring)
-    outerdict["stuff"].append(innerdict)  
+        outerdict["stuff"].append(innerdict)  
 with open("sample.json", "w") as outfile:
     json.dump(outerdict, outfile)
     
